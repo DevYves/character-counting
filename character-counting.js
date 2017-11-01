dc76b4cee323dc202f43a6f5b4648167e1ce6d0d
DevYves <yfreypons@gmail.com>
2017-11-01 19:40:22 +0000

    Working function to list characters using objects

---
+++ character-counting.js
@@ -0,0 +1,30 @@
+// //input -> string
+
+// //output -> returns all the unique characters that exist in the string && how many instances of each letter
+// // were found in the string
+// // var noSpaces = string.split(" ").join("");
+// //   var mid = Math.ceil(noSpaces.length/2);
+function NewStringBuilder(stringClone){
+  var noSpaces = stringClone.split(" ").join("").toLowerCase();
+
+  return noSpaces;
+}
+// console.log(NewStringBuilder("This is a string with no spaces"));
+
+function countLetters(noSpacesString){
+  letterCount ={};
+  for (var i = 0; i < noSpacesString.length; i++) {
+  var character = noSpacesString[i];
+
+    if (letterCount[character]) {
+      letterCount[character]++
+    } else {
+       letterCount[character] = 1;
+      }
+  }
+return letterCount;
+}
+
+
+console.log(countLetters(NewStringBuilder("This is a string without spaces")));
+
